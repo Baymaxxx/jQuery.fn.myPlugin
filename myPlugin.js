ac8f0d684270ec51fc7c5934127a7de5b2b70d62
baymaxxx <81243232@qq.com>
2016-08-01 13:39:25 +0800

    .

--- myPlugin.js
+++ myPlugin.js
@@ -2,13 +2,12 @@
  * Created by zhongshuai on 2016/8/1.
  */
 
-jQuery.fn.myPlugin = function () {
+(function ($) {
     $.fn.maxHeight = function () {
         var max = 0;
         this.each(function () {
-            max = Math.max(max,$(this).height());
+            max = Math.max(max, $(this).height());
         });
         return max;
-    }
-};
-
+    };
+})(jQuery);
